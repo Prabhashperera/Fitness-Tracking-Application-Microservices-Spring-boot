50d28abe7a8e2407f459a4a41cc97abf2d4b7004
Prabhashperera <prabashmperera01@gmail.com>
2026-02-10 23:01:55 +0530

    Activity Service Implementation

--- ActivityService/src/main/java/com/fitness/activityservice/controller/ActivityController.java
+++ ActivityService/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -3,16 +3,16 @@ package com.fitness.activityservice.controller;
 import com.fitness.activityservice.dto.ActivityRequest;
 import com.fitness.activityservice.dto.ActivityResponse;
 import com.fitness.activityservice.service.ActivityService;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/activity")
-@RequiredArgsConstructor
+@RequestMapping("/api/activity/")
+@AllArgsConstructor
 public class ActivityController {
     private ActivityService activityService;
 
@@ -20,5 +20,10 @@ public class ActivityController {
     @PostMapping
     public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request) {
         return ResponseEntity.ok(activityService.trackActivity(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ActivityResponse>> getUserActivities(@RequestHeader("X-User-ID") String userId) {
+        return ResponseEntity.ok(activityService.getUserActivities(userId));
     }
 }
--- ActivityService/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ ActivityService/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -1,4 +1,62 @@
 package com.fitness.activityservice.service;
 
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import com.fitness.activityservice.repo.ActivityRepo;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ActivityService {
+    private final ActivityRepo activityRepo;
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+        Activity activity = Activity.builder()
+                .userID(request.getUserID())
+                .activityType(request.getActivityType())
+                .duration(request.getDuration())
+                .caloriesBurned(request.getCaloriesBurned())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+        Activity savedActivity = activityRepo.save(activity);
+        return mapToResponse(savedActivity);
+    }
+
+    public ActivityResponse mapToResponse(Activity activity) {
+        ActivityResponse response = new ActivityResponse();
+        response.setId(activity.getId());
+        response.setActivityType(activity.getActivityType());
+        response.setDuration(activity.getDuration());
+        response.setCaloriesBurned(activity.getCaloriesBurned());
+        response.setStartTime(activity.getStartTime());
+        response.setAdditionalMetrics(activity.getAdditionalMetrics());
+        return response;
+    }
+
+    public List<ActivityResponse> getUserActivities(String userId) {
+        List<Activity> dataList = activityRepo.findByUserID(userId);
+        List<ActivityResponse> convertedActivities = new ArrayList<>();
+
+        for (Activity activity : dataList) {
+            ActivityResponse response = new ActivityResponse();
+            response.setUserID(activity.getUserID());
+            response.setActivityType(activity.getActivityType());
+            response.setDuration(activity.getDuration());
+            response.setCaloriesBurned(activity.getCaloriesBurned());
+            response.setStartTime(activity.getStartTime());
+            response.setAdditionalMetrics(activity.getAdditionalMetrics());
+
+            convertedActivities.add(response);
+        }
+
+        return convertedActivities;
+    }
 }
